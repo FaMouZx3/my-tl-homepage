8f59fca169af7a8ec69530540e9d68e0a1b32156
Tim Famouz <famouzx3@gmx.de>
2022-07-11 22:52:16 +0200

    Added social media bar

--- src/Modules/ui/ui-profile-info/ProfileInfo.tsx
+++ src/Modules/ui/ui-profile-info/ProfileInfo.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import SocialMediaBar from "./components/ui-social-media-bar/SocialMediaBar";
 import "./ProfileInfo.scss";
 
 const ProfileInfo = () => {
     return (
         <div className="profile-container">
-            
+            <div/>
+            <div/>
+            <SocialMediaBar/>
         </div>
     );
 }
